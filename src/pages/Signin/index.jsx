505eac87094518126fb0fd74548cb357f8b0fc4d
Danilo Ataide <danilosataide@gmail.com>
2022-10-23 15:39:00 -0300

    @react-native-material Icon Button e ajustes no layout

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,30 +1,47 @@
 import React from 'react'
-import { View, Text, StyleSheet , TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet , TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native'
 import * as Animatable from 'react-native-animatable';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { IconButton } from '@react-native-material/core';
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
+import LogoPlaneta from '../../assets/logoPlaneta.png';
 
 export default function SignIn() {
+  const image = { uri: "https://www.construtoraplaneta.com.br/wp-content/uploads/2022/01/fundo-sus-02.jpg" };
   const navigation  = useNavigation()
 
   return (
     <View style={styles.container}>
+      <ImageBackground source={image} style={styles.image}>
       <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+        <Image  source={LogoPlaneta} 
+          style={styles.imageSecondary} resizeMode="contain"
+        />
         <Text style={styles.message}>Bem vindo(a)!</Text>
       </Animatable.View>
 
       <Animatable.View animation="fadeInUp" style={styles.containerForm}>
-        <Text style={styles.title}>E-mail</Text>
+        {/* <Text style={styles.title}>E-mail</Text> */}
         <TextInput 
           placeholder="E-mail"
           style={styles.input}
         />
 
-        <Text style={styles.title}>Senha</Text>
-        <TextInput 
-          placeholder="Senha"
-          style={styles.input}
-        />
+        {/* <Text style={styles.title}>Senha</Text> */}
+        
+        <View
+          style={styles.inputSenha}
+        >
+          <TextInput 
+            placeholder="Senha"
+            style={styles.inputSenhaText}
+          />
+          <IconButton style={styles.icon} icon={props => <FontAwesome name="eye" {...props} size={16}/>} />
+          {/* <FontAwesome style={styles.icon} name="chevron-right" size={16}/> */}
+        </View>
+
 
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Entrar</Text>
@@ -33,8 +50,9 @@ export default function SignIn() {
         <TouchableOpacity style={styles.buttonRegister}>
           <Text style={styles.registerText}>Cadastre-se</Text>
         </TouchableOpacity>
-
+        
       </Animatable.View>
+      </ImageBackground>
     </View>
   )
 }
@@ -44,10 +62,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#14750D', //#228b1b
   },
+  image: {
+    flex: 1
+  },
   containerHeader: {
     marginTop: '14%',
     marginBottom: 25,
-    paddingStart: 50,
+    paddingStart: 40,
   },
   message: {
     fontSize: 28,
@@ -57,22 +78,79 @@ const styles = StyleSheet.create({
   containerForm: {
     backgroundColor: '#FFF',
     flex: 1,
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    paddingStart: 50,
-    paddingEnd: 50
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    paddingStart: 40,
+    paddingEnd: 40,
+    paddingTop: 28,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     marginTop: 28,
   },
   input: {
-    borderBottomWidth: 1,
-    height: 40,
+    borderBottomWidth: 0,
+    height: 50,
+    color: 'black',//#3B8952
+    fontWeight: 'normal',
     marginBottom: 12,
-    fontSize: 16
+    fontSize: 16,
+    backgroundColor: '#f3f3f3',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5
+  },
+  inputSenha: {
+    borderBottomWidth: 0,
+    height: 50,
+    color: 'black',//#3B8952
+    fontWeight: 'normal',
+    marginBottom: 12,
+    backgroundColor: '#f3f3f3',
+    paddingStart: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  inputSenhaText: {
+    fontSize: 16,
+    width: '85%',
+  },
+  icon: {
+    width: '15%',
+    paddingHorizontal: 15
   },
   button: {
-    backgroundColor: '#14750D'
-  }
+    backgroundColor: '#14750D',
+    width: '100%',
+    height: 50,
+    borderRadius: 50,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonRegister: { 
+    marginTop: 14,
+    alignSelf: 'center',
+  },
+  registerText: {
+    color: '#a1a1a1',
+    fontSize: 16
+  },
+  imageSecondary: {
+    width: 140, 
+    height: 140,
+    backgroundColor: 'transparent', 
+  },
 })
--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native' 
+import { View, Text, StyleSheet, Image, TouchableOpacity, ActivityIndicator, ImageBackground } from 'react-native' 
 import { useNavigation } from '@react-navigation/native'
-
+import { FontAwesome } from '@expo/vector-icons';
 import * as Animatable from 'react-native-animatable';
-import fundoSusten from '../../assets/fundo_susten.webp'
+
+import Logo from '../../../src/assets/logo.png';
+import LogoPlaneta from '../../../src/assets/logoPlaneta.png';
 
 export default function Welcome() {
   const navigation  = useNavigation()
-  const image = { uri: "https://www.construtoraplaneta.com.br/wp-content/uploads/2022/01/fundo-sus-02.jpg" };
 
+  const image = { uri: "https://www.construtoraplaneta.com.br/wp-content/uploads/2022/01/fundo-sus-02.jpg" };
+  // const LogoPlaneta = { uri: "https://www.construtoraplaneta.com.br/wp-content/themes/planeta/imagens/logo.svg" };
+  // const Logo = require('../../assets/logo_design.svg')
+  
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
-        <Animatable.Image  source={{uri:'https://www.construtoraplaneta.com.br/wp-content/themes/planeta/imagens/logo.svg'}} 
+        <Animatable.Image  source={LogoPlaneta} 
           style={styles.imageSecondary} resizeMode="contain"
         />
       <View style={styles.containerLogo}>
@@ -21,7 +26,7 @@ export default function Welcome() {
           /> */}
         <Animatable.Image
           animation="flipInY"
-          source={require('../../assets/logo_design.svg')}
+          source={Logo}
           style = {{ width: "50%", height: "50%", backgroundColor: 'transparent' }} resizeMode='contain'
         />
       </View>
@@ -38,7 +43,10 @@ export default function Welcome() {
           style={styles.button} 
           onPress={() => navigation.navigate('SignIn')}
         >
-          <Text style={styles.buttonText}>Acessar</Text>
+          <Text style={styles.buttonText}>
+            Acessar
+          </Text>
+          <FontAwesome style={styles.buttonIcon} name="chevron-right" size={16}/>
         </TouchableOpacity>
       </Animatable.View>
       </ImageBackground>
@@ -56,8 +64,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   imageSecondary: {
-    width: 100, 
-    height: 100,
+    width: 120, 
+    height: 120,
     backgroundColor: 'transparent', 
     marginLeft: 25,
   },
@@ -71,34 +79,43 @@ const styles = StyleSheet.create({
   containerForm: {
     flex: 2,
     backgroundColor: '#fff',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
     paddingStart: '5%',
     paddingEnd: '5%'
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     marginTop: 28,
     marginBottom: 12
   },
   text: {
+    fontSize: 18,
     color: 'gray'
   },
   button: {
     position: 'absolute',
     backgroundColor: '#14750D',
+    height: 50,
     borderRadius: 50,
     paddingVertical: 8,
-    width: '60%',
+    width: '60%', 
     alignSelf: 'center',
-    bottom: '15%',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: 'row',
+    bottom: '15%',
   },
   buttonText: {
     fontSize: 18,
     color: '#fff',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    paddingEnd: 5
+  },
+  buttonIcon: {
+    color: "#fff",
+    paddingStart: 5,
+    alignSelf: 'center'
   }
 })
